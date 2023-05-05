0210a28e51c3b4f424d1b2af52c3d3eb075383d6
NH94 <121815194+NH94@users.noreply.github.com>
2023-05-05 22:27:48 +0300

    Update mpibmf.cpp

--- mpibmf.cpp
+++ mpibmf.cpp
@@ -1,8 +1,4 @@
-/*
- * This is a mpi version of bellman_ford algorithm
- * Compile: mpic++ -std=c++11 -o mpi_bellman_ford mpi_bellman_ford.cpp
- * Run: mpiexec -n <number of processes> ./mpi_bellman_ford <input file>, you will find the output file 'output.txt'
- * */
+
 
 #include <string>
 #include <cassert>
@@ -11,7 +7,6 @@
 #include <algorithm>
 #include <iomanip>
 #include <cstring>
-
 #include "mpi.h"
 
 using std::string;
@@ -21,7 +16,7 @@ using std::endl;
 #define INF 1000000
 
 /**
- * utils is a namespace for utility functions
+ =
  * including I/O (read input file and print results) and matrix dimension convert(2D->1D) function
  */
 namespace utils {
@@ -44,7 +39,7 @@ namespace utils {
             abort_with_error_message("ERROR OCCURRED WHILE READING INPUT FILE");
         }
         inputf >> N;
-        //input matrix should be smaller than 20MB * 20MB (400MB, we don't have too much memory for multi-processors)
+    
         assert(N < (1024 * 1024 * 20));
         mat = (int *) malloc(N * N * sizeof(int));
         for (int i = 0; i < N; i++)
@@ -55,7 +50,7 @@ namespace utils {
     }
 
     int print_result(bool has_negative_cycle, int *dist) {
-        std::ofstream outputf("output.txt", std::ofstream::out);
+        std::ofstream outputf("outtxt", std::ofstream::out);
         if (!has_negative_cycle) {
             for (int i = 0; i < N; i++) {
                 if (dist[i] > INF)
@@ -64,24 +59,12 @@ namespace utils {
             }
             outputf.flush();
         } else {
-            outputf << "FOUND NEGATIVE CYCLE!" << endl;
+            outputf << "NEGATIVE CYCLE!" << endl;
         }
         outputf.close();
         return 0;
     }
 }//namespace utils
-
-
-/**
- * Bellman-Ford algorithm. Find the shortest path from vertex 0 to other vertices.
- * @param my_rank the rank of current process
- * @param p number of processes
- * @param comm the MPI communicator
- * @param n input size
- * @param *mat input adjacency matrix
- * @param *dist distance array
- * @param *has_negative_cycle a bool variable to recode if there are negative cycles
-*/
 void bellman_ford(int my_rank, int p, MPI_Comm comm, int n, int *mat, int *dist, bool *has_negative_cycle) {
     int loc_n; // need a local copy for N
     int loc_start, loc_end;
